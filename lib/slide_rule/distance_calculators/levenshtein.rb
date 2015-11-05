f7b343ccd517fd4f78d579c2f3a91625d3dff4b7
Joey Ferguson <fergmastaflex@gmail.com>
2015-11-05 09:30:59 -0700

    Add normalized distance

--- lib/slide_rule/distance_calculators/levenshtein.rb
+++ lib/slide_rule/distance_calculators/levenshtein.rb
@@ -2,14 +2,13 @@ module SlideRule
   module DistanceCalculators
     class Levenshtein
       def calculate(first, second)
-        distance = ::Vladlev.distance(first, second).to_f
+        distance = ::Vladlev.get_normalized_distance(first, second).to_f
 
         # Lower bound is difference in length
         # distance = matrix.last.last.to_f - (first.length - second.length).abs
 
         # Upper bound is length of longest string
         # This will decrease distance more for longer strings.
-        distance / ([first.length, second.length].max)
       end
     end
   end
