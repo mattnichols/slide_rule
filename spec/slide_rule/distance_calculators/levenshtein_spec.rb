d9a35bfc31906a7446d662ca17edca892049964e
Joey Ferguson <fergmastaflex@gmail.com>
2015-11-05 09:31:10 -0700

    Fix specs for normalized_distance

--- spec/slide_rule/distance_calculators/levenshtein_spec.rb
+++ spec/slide_rule/distance_calculators/levenshtein_spec.rb
@@ -6,6 +6,6 @@ describe ::SlideRule::DistanceCalculators::Levenshtein do
   end
 
   it 'should calculate distance as distance divided by length of longest string' do
-    expect(described_class.new.calculate('this is a test', 'this is a test!')).to eq(1.0 / 15)
+    expect(described_class.new.calculate('this is a test', 'this is a test!').round(4)).to eq((1.0 / 15).round(4))
   end
 end
